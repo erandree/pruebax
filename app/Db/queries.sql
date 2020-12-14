568433992e33f4ac49e35b2f7a666862087cd2ce
erandree <ernesto_031@hotmail.com>
2020-12-14 15:38:23 -0500

    Ready

--- app/Db/queries.sql
+++ app/Db/queries.sql
@@ -1,33 +1,92 @@
-CREATE TABLE `aplicacionz`.`clientes` (
-  `idcliente` INT NOT NULL AUTO_INCREMENT,
-  `identificacion` VARCHAR(45) NOT NULL,
-  `primer_nombre` VARCHAR(45) NOT NULL,
-  `segundo_nombre` VARCHAR(45) NULL,
-  `primer_apellido` VARCHAR(45) NOT NULL,
-  `segundo_apellido` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idcliente`));
+CREATE TABLE `actividades` (
+  `id` int(4) NOT NULL,
+  `fk_proyecto` int(4) NOT NULL,
+  `lugar` varchar(50) NOT NULL,
+  `descripcion_lugar` varchar(60) NOT NULL,
+  `actividad` varchar(50) NOT NULL,
+  `horas` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `funciones` (
+  `id` int(4) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `controlador` varchar(25) NOT NULL,
+  `action` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into clientes (identificacion, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido)
-values ('8-000-0000','Ana', 'Rosa', 'Vega', 'Santos'),
-('4-000-0000','Marcos', 'Miguel', 'Castro', 'Estrada');
+CREATE TABLE `participantes` (
+  `id` int(4) NOT NULL,
+  `cedula` varchar(20) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `apellido` varchar(25) NOT NULL,
+  `correoutp` varchar(50) NOT NULL,
+  `contrasena` varchar(20) NOT NULL,
+  `celular` varchar(12) DEFAULT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
+  `ultima_modificacion` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `aplicacionz`.`ventas` (
-  `idventa` INT NOT NULL AUTO_INCREMENT,
-  `cliente` INT NOT NULL,
-  `total` DECIMAL(8,2) NOT NULL,
-  `puntos` INT NULL,
-  PRIMARY KEY (`idventa`),
-  INDEX `fk_cliente` (`cliente` ASC),
-  CONSTRAINT `fk_clienteventas`
-    FOREIGN KEY (`cliente`)
-    REFERENCES `aplicacionz`.`clientes` (`idcliente`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `proyectos` (
+  `id` int(4) NOT NULL,
+  `tipo` varchar(30) NOT NULL,
+  `nivel` varchar(30) NOT NULL,
+  `clasificacion` varchar(50) NOT NULL,
+  `categoria` varchar(75) NOT NULL,
+  `modalidad` varchar(30) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `estado` varchar(15) NOT NULL,
+  `direccionimg` varchar(100) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `objetivo` varchar(100) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `proponente` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO ventas (cliente_id, total, puntos)
-VALUES(1, 1500.95, 100),
-(2, 10000.90, 510);
+CREATE TABLE `proyectosparticipantes` (
+  `id_proyecto` int(5) NOT NULL,
+  `id_participante` int(5) NOT NULL,
+  `funcion` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `actividades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_actividades_proyectos` (`fk_proyecto`);
+
+  ALTER TABLE `funciones`
+  ADD PRIMARY KEY (`id`);
+
+  ALTER TABLE `participantes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correoutp` (`correoutp`),
+  ADD UNIQUE KEY `cedula` (`cedula`),
+  ADD UNIQUE KEY `celular` (`celular`) USING BTREE;
+
+  ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`);
+
+  ALTER TABLE `proyectosparticipantes`
+  ADD UNIQUE KEY `registro_unico` (`id_proyecto`,`id_participante`),
+  ADD KEY `fk_idparticipantes` (`id_participante`);
+
+  ALTER TABLE `actividades`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+  ALTER TABLE `funciones`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+  ALTER TABLE `participantes`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+  ALTER TABLE `proyectos`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+  ALTER TABLE `actividades`
+  ADD CONSTRAINT `fk_actividades_proyectos` FOREIGN KEY (`fk_proyecto`) REFERENCES `proyectos` (`id`);
+
+  ALTER TABLE `proyectosparticipantes`
+  ADD CONSTRAINT `fk_idparticipantes` FOREIGN KEY (`id_participante`) REFERENCES `participantes` (`id`),
+  ADD CONSTRAINT `fk_idproyecto` FOREIGN KEY (`id_proyecto`) REFERENCES `proyectos` (`id`);
+COMMIT;
