c9dc02971322c5bcda0f6b798cd971ab7a8ebab1
erandree <ernesto_031@hotmail.com>
2020-12-17 17:59:42 -0500

    Update asignar.php

--- app/Views/Proyecto/Participantes/asignar.php
+++ app/Views/Proyecto/Participantes/asignar.php
@@ -33,7 +33,7 @@
                                         <?php endforeach ?>
                                     </select>
                                     <div class="invalid-feedback">
-                                        *Por favor seleccione el correo del participante
+                                        *Por favor seleccione el correo del participante.
 										</div>
                                     
                                 
@@ -50,7 +50,7 @@
                                             <option value="Participante">Participante</option>
                                         </select>
                                         <div class="invalid-feedback">
-                                        *Debe asignar una función al participante
+                                        *Debe asignar una función al participante.
 										</div>
                                 </div>
                             
